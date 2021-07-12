39210cfb83a551f92b4d9cd1b4af56a81f7dd935
SYEDHAROONA <6210110650@psu.ac.th>
2021-07-12 11:27:20 +0700

    Components and Image Background

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Wheather';
+import Constants from 'expo-constants';
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Weather zipCode="90110"/>
+    <StatusBar style="auto" />
+    </View>
+ );
+}
+
+const styles = StyleSheet.create({
+ container: {
+     paddingTop: Constants.statusBarHeight
+ },
+});
