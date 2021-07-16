531de39cf4a93b773829bd71bf6f36b0639c4771
SYEDHAROONA <6210110650@psu.ac.th>
2021-07-16 14:57:30 +0700

    Hello world

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Weather from './components/Wheather';
-import Constants from 'expo-constants';
+import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
-  return (
-    <View style={styles.container}>
-    <Weather zipCode="90110"/>
-    <StatusBar style="auto" />
-    </View>
+ const doIt = () => {
+ console.log("Hello from console")
+ }
+ return (
+ <View style={styles.container}>
+ <Text onPress={doIt}>Hello world</Text>
+ <StatusBar style="auto" />
+ </View>
  );
 }
-
 const styles = StyleSheet.create({
  container: {
-     paddingTop: Constants.statusBarHeight
+ flex: 1,
+ backgroundColor: '#fff',
+ alignItems: 'center',
+ justifyContent: 'center',
  },
 });
-ls;akdjf;lk
